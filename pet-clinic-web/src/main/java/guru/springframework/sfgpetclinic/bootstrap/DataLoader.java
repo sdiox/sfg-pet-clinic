75ab2c59eaba0e9f38380af06ac574029ccf98f3
sdiox <dontbe@live.jp>
2020-07-28 23:26:46 +0700

    Fix mistakes in DataLoader and modify owner index page

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -28,7 +28,7 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(1L);
+        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
 
@@ -37,7 +37,7 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loading Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(2L);
+        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
